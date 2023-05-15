0ed0c31fbdbe7356e1d21d5fcbbfc399cdfc9a07
ice-wzl <anonymous@anon.com>
2023-05-15 16:35:26 -0400

    Red divider line will now be terminal width

--- clip.py
+++ clip.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+import os
 import pyperclip
 from termcolor import cprint 
 
 while True:
     buffer_wait = pyperclip.waitForNewPaste()
-    cprint("------------------------------------------------------------------", "red")
+    cprint("-" * os.get_terminal_size()[0], "red")
     print(buffer_wait.strip())
