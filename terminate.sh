2faad474095e629a4330ebde6294aa3eb760ccdc
ice-wzl <75596877+ice-wzl@users.noreply.github.com>
2023-09-21 22:49:40 -0400

    updates

--- terminate.sh
+++ terminate.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-terminator -p default --geometry 960x1000 -T "target" &
-terminator -p default --geometry 960x1000 -T "target" &
+#terminator -p default --geometry 960x1000 -T "target" &
+#terminator -p default --geometry 960x1000 -T "target" &
 terminator -p local-alt --geometry 800x200 -T "clip-monitor" -e "python3 /opt/red-team-tools/clip.py" &
 terminator -p local-alt --geometry 800x400 -T "cred-manager" -e "python3 /opt/red-team-tools/cred-manager.py" &
 terminator -p local --geometry 960x1000 -T "local" &
 terminator -p local-alt -T "bandwidth monitor **LIMIT 300MB***" --geometry 375x125 -e "python3 /opt/red-team-tools/bandwidth.py -n -p 10" &
+/usr/bin/tmux
 
