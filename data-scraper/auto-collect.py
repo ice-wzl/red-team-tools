a0c931d81242de609ad4faed030d37bf84a866c1
ice-wzl <75596877+ice-wzl@users.noreply.github.com>
2023-12-03 21:40:16 -0500

    major updates, vps ready

--- data-scraper/auto-collect.py
+++ data-scraper/auto-collect.py
@@ -8,8 +8,12 @@ from time import sleep
 from colorama import Fore, Back, Style
 from requests.exceptions import ConnectionError, Timeout, RequestException
 from urllib3.exceptions import ConnectionError
+from datetime import datetime
 
+now = datetime.now()
+date_time = now.strftime("%m-%d-%Y-%H:%M:%S")
 
+# prints the banner
 def banner():
     print(
         """
@@ -40,6 +44,11 @@ def banner():
 
 
 def setup_api():
+    """
+    Function sets up the api key for use later on in the script. Will read from the file api.txt containing the key
+
+    Return: returns back the api key in the variable name api
+    """
     # should read api key from a file to prevent me from blundering my api key in a git commit
     with open("api.txt", "r") as fp:
         api_key = fp.read().strip()
@@ -49,6 +58,11 @@ def setup_api():
 
 
 def do_query(api, query):
+    """
+    Function takes in an api key and a query to perform. 
+    Will conduct the query against shodan and print out only the ip addresses into a file called results.txt
+    Has no return value 
+    """
     # Perform the search
     query = "".join(query)
     print("Query {}".format(query))
@@ -61,6 +75,11 @@ def do_query(api, query):
 
 
 def get_tor_session():
+    """
+    Function has no input parameters. It will get up a tor session to make requests to webservers over tor vice your own ip
+
+    Return: Returns back the session for use later on in the script
+    """
     session = requests.session()
     # Tor uses the 9050 port as the default socks port
     session.proxies = {"http": "socks5://127.0.0.1:9050"}
@@ -68,6 +87,16 @@ def get_tor_session():
 
 
 def do_request():
+    """
+    Function performs a sanity check against icanhazip.com to ensure you are not leaking your true public ip 
+    Function with read from results.txt containing our ip addresses 
+    It will iterate through all the results in the results.txt file and make a request to the ip on port 8000
+    It will get the site content html content back and save it in variable site_content 
+    It will check if the ip already exists in our history.txt file, by calling check_exist()
+    If it is not in history.txt it will make a call to key_words() to check the html content for key words
+
+    Return: none
+    """
     # sets up tor socks port as default proxy
     session = get_tor_session()
     # Tor uses the 9050 port as the default socks port
@@ -88,6 +117,7 @@ def do_request():
         with requests.sessions.Session() as session:
             for ip in read_in:
                 try:
+                    # line below is redundant, should not be needed as it is already defined above 
                     session = get_tor_session()
                     response = session.get(
                         "http://{}:8000/".format(ip.strip()),
@@ -101,6 +131,12 @@ def do_request():
                             ip.strip(), response.status_code
                         )
                     )
+
+                    handle = open("session_history" + date_time, "a+")
+                    handle.write("Made request to {}, Status Code: {}\n".format(
+                            ip.strip(), response.status_code) + Fore.RESET)
+                    handle.close()
+                    
                     sleep(5.0)
                     # set the request content to a variable for parsing
                     site_content = response.content
@@ -111,6 +147,10 @@ def do_request():
                 # many exceptions to try and handle gracefully
                 except (ConnectionError, Timeout, RequestException):
                     print(Fore.RED + "{}, Host not responsive".format(ip.strip()))
+                    
+                    handle = open("session_history" + date_time, "a+")
+                    handle.write(Fore.RED + "{}, Host not responsive\n".format(ip.strip()) + Fore.RESET)
+                    handle.close()
 
 
 def key_words(content, ip_addr):
@@ -174,11 +214,17 @@ def key_words(content, ip_addr):
         if i in content:
             i = i.decode()
             print(Fore.GREEN + "\t{} found at {}".format(i, ip_addr.strip()))
+
+            handle = open("session_history" + date_time, "a+")
+            handle.write(Fore.GREEN + "\t{} found at {}\n".format(i, ip_addr.strip()) + Fore.RESET)
+            handle.close()
+
     if b".ssh/" in content:
         crawl_ssh(ip_addr)
 
 
 def crawl_ssh(ip_addr):
+    interesting_words = [b"ecdsa", b"rsa", b"pub", b"dsa", b"ecdsa_sk", b"ed25519", b"ed25519_sk"]
     try:
 
         with requests.sessions.Session() as session:
@@ -193,10 +239,23 @@ def crawl_ssh(ip_addr):
                 Fore.RESET
                 + "Made request to {}, Status Code: {}".format(
                     ip_addr.strip(), response.status_code
-                )
-            )
+                ))
+            
+            handle = open("session_history" + date_time, "a+")
+            handle.write(Fore.RESET + "Made request to {}, Status Code: {}\n".format(ip_addr.strip(), response.status_code))
+            handle.close()
+
             site_content = response.content
-            print(site_content)
+            # debugging line uncomment below 
+            # print(site_content)
+            for i in interesting_words:
+                if i in site_content:
+                    i = i.decode()
+                    print(Fore.GREEN + "\t\t{} found at http://{}:8000/.ssh".format(i, ip_addr))
+                    
+                    handle = open("session_history" + date_time, "a+")
+                    handle.write(Fore.GREEN + "\t\t{} found at http://{}:8000/.ssh\n".format(i, ip_addr))
+                    handle.close()
 
     except (ConnectionError, Timeout, RequestException):
         print(Fore.RED + "{}, Host not responsive".format(ip_addr.strip()))
@@ -211,6 +270,11 @@ def check_exist(ip_addr):
                 exists = 1
         if exists == 1:
             print("\t{} is in our history file".format(ip_addr.strip("\n")))
+
+            handle = open("session_history" + date_time, "a+")
+            handle.write("\t{} is in our history file\n".format(ip_addr.strip("\n")))
+            handle.close()
+            
             return True
         else:
             # add_entry(ip_addr)
@@ -221,7 +285,11 @@ def check_exist(ip_addr):
 
 
 if __name__ == "__main__":
+    # print the banner
     banner()
+    # conduct the shodan query to get the results
     do_query(setup_api(), 'Title:"Directory listing for /" port:8000')
+    # sleeps for 5 seconds, should not be needed, test if you can remove this 
     sleep(5.0)
+    # perform the requests which will loop through the results in results.txt
     do_request()
