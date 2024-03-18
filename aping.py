4e4d6de7a782865819cd2645d3f6cd52c0c4ebce
ice-wzl <75596877+ice-wzl@users.noreply.github.com>
2024-03-18 14:05:00 -0400

    updates

--- aping.py
+++ aping.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3 
+import os
 import sys
-from scapy.all import srp,Ether,ARP,conf
+try:
+    from scapy.all import srp,Ether,ARP,conf
+except ModuleNotFoundError:
+    print("[!] Scapy not found...pip3 install scapy")
+    sys.exit(3)
+
 
 if len(sys.argv) != 2:
     print("Usage aping.py <network>/CIDR")
     print(" --> python3 arping.py 10.10.10.0/24")
+    print(" --> python3 arping.py 192.168.15.1")
     sys.exit(1)
+
+if os.getuid() != 0:
+    print("[!] Script must be run as root...")
+    sys.exit(2)
 
 conf.verb = 0
 ans,unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=sys.argv[1]), timeout=3)
 for s,r in ans:
-    print(r.sprintf("%Ether.src% %ARP.psrc%"))
-
-
+    print(r.sprintf("%Ether.src% <--> %ARP.psrc%"))
 
 
 
